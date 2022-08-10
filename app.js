40330913a4003d4b372aabaf344bf74b24ea69ea
Bruno Martins <bruno.martins217@gmail.com>
2022-08-09 21:36:36 -0300

    express config

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http = require('http');
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express!</h1>');
+});
+
+app.listen(3000);
