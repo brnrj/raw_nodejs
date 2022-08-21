8656466fc786661fd800164645471b17fe39f6bc
Bruno Martins <bruno.martins217@gmail.com>
2022-08-21 15:26:38 -0300

    implementing pug

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const { products, router: adminRoutes } = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
