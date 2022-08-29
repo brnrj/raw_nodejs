cebe4c8b8a4da51445208ea94ae266eaf5d20e59
Bruno Martins <bruno.martins217@gmail.com>
2022-08-29 03:03:53 -0300

    refactoring code

--- models/product.js
+++ models/product.js
@@ -1,10 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+
 const p = path.join(
   path.dirname(require.main.filename),
   'data',
   'products.json'
 );
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+}
 
 module.exports = class Product {
   constructor(t) {
@@ -12,11 +23,7 @@ module.exports = class Product {
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -25,12 +32,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        return cb([]);
-      } else {
-        return cb(JSON.parse(fileContent));
-      }
-    });
+    getProductsFromFile(cb);
   }
 };
