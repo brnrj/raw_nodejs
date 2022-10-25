9b34de9f32c0890f94ef8c9f91783d28176d5e5e
Bruno Martins <bruno.martins217@gmail.com>
2022-10-24 22:16:50 -0300

    linking to the edit page and editing the product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,7 @@ exports.getAddProduct = (req, res, next) => {
   
   exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, price, description } = req.body;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect('/');
   };
@@ -33,6 +33,13 @@ exports.getAddProduct = (req, res, next) => {
         product: product
     });
     })
+  };
+  
+  exports.postEditProduct = (req, res, next) => {
+    const { productId, title, imageUrl, description, price } = req.body;
+    const updatedProduct = new Product(productId, title, imageUrl, description, price);
+    updatedProduct.save();
+    res.redirect('/admin/products');
   };
 
   exports.getProducts = (req, res, next) => {
